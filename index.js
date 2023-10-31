6e7199ed468815200b019d4b46964236d7e54157
Victor <victor.castanos@gmail.com>
2023-10-31 18:06:03 +0100

    lab

---
+++ index.js
@@ -0,0 +1,37 @@
+console.log("js is working");
+//"https://pokeapi.co/api/v2/pokemon/"
+
+fetch("https://pokeapi.co/api/v2/pokemon/")
+    .then((response) => response.json())
+    .then((data) => {
+        printData(data)
+        fetch(data.results[0].url)
+        .then(response => response.json())
+        .then(pokemonData => { 
+            const elementImg = document.createElement('img');
+            elementImg.setAttribute("src",pokemonData.sprites.front_default)
+            const li = document.getElementById(pokemonData.sprites.front_default);
+            li.appendChild(elementImg);
+            
+            
+            console.log(pokemonData.sprites.back_default);
+            
+        })
+    })
+
+    .catch((error) => { console.log(error); })
+    
+    function printData(data){
+        
+        const ulElement = document.querySelector("#list");
+        data.results.forEach(element => {
+            const liElement = document.createElement("li");
+            
+            liElement.id = element.name
+            liElement.innerHTML = `<h2>${element.name}</h2>`
+            ulElement.appendChild(liElement);
+            console.log(element);
+        });
+    }
+
+    
