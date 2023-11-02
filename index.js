efc2cd49b0b1512e61765fa4db959caf7d927cc1
Victor <victor.castanos@gmail.com>
2023-11-02 14:46:22 +0100

    done

--- index.js
+++ index.js
@@ -6,32 +6,35 @@ fetch("https://pokeapi.co/api/v2/pokemon/")
     .then((data) => {
         printData(data)
         fetch(data.results[0].url)
-        .then(response => response.json())
-        .then(pokemonData => { 
-            const elementImg = document.createElement('img');
-            elementImg.setAttribute("src",pokemonData.sprites.front_default)
-            const li = document.getElementById(pokemonData.sprites.front_default);
-            li.appendChild(elementImg);
-            
-            
-            console.log(pokemonData.sprites.back_default);
-            
-        })
+            .then(response => response.json())
+            .then(pokemonData => {
+                const elementImg = document.createElement('img');
+                elementImg.setAttribute("src", pokemonData.sprites.front_default)
+                const li = document.getElementById(pokemonData.sprites.front_default);
+                li.appendChild(elementImg);
+
+
+                console.log(pokemonData.sprites.back_default);
+
+            })
     })
-
     .catch((error) => { console.log(error); })
-    
-    function printData(data){
-        
-        const ulElement = document.querySelector("#list");
-        data.results.forEach(element => {
-            const liElement = document.createElement("li");
-            
-            liElement.id = element.name
-            liElement.innerHTML = `<h2>${element.name}</h2>`
-            ulElement.appendChild(liElement);
-            console.log(element);
-        });
-    }
+function printData(data) {
 
-    
+    const ulElement = document.querySelector("#list");
+    data.results.forEach(element => {
+        fetch(element.url)
+            .then((response) => {
+                return response.json()
+            })
+            .then((pokemonData) => {
+                const imageURL = pokemonData.sprites.front_default
+            })
+        const liElement = document.createElement("li");
+        liElement.id = element.name
+        liElement.innerHTML = `<h2>${element.name} <img src=${imageURL}</h2>`
+        ulElement.appendChild(liElement);
+        console.log(element);
+    });
+}
+
